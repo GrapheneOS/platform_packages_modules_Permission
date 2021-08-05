d485f13f89e8bdb805d7cdfccbc85c6af6f23d8a
Sumit Bhagwani <bhagwani@google.com>
2021-08-05 13:42:24 +0000

    Removing unused code from PermissionHistoryPreference
    This was added to show detailed time usages and was later shelved.
    
    Bug: 191048348
    
    Test: N/A
    Change-Id: I1bc2e592f08c966c61a9962c2248f223fab57fbc

--- PermissionController/src/com/android/permissioncontroller/permission/ui/handheld/dashboard/PermissionHistoryPreference.java
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/handheld/dashboard/PermissionHistoryPreference.java
@@ -20,7 +20,6 @@ import static com.android.permissioncontroller.PermissionControllerStatsLog.PERM
 import static com.android.permissioncontroller.PermissionControllerStatsLog.PERMISSION_DETAILS_INTERACTION__ACTION__INFO_ICON_CLICKED;
 import static com.android.permissioncontroller.PermissionControllerStatsLog.write;
 
-import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -28,15 +27,11 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.UserHandle;
-import android.text.format.DateFormat;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -45,11 +40,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.permissioncontroller.R;
-import com.android.permissioncontroller.permission.utils.KotlinUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,8 +61,6 @@ public class PermissionHistoryPreference extends Preference {
     private final String mAccessTime;
     private final Drawable mAppIcon;
     private final String mTitle;
-    private final float mDialogWidthScalar;
-    private final float mDialogHeightScalar;
     private final List<Long> mAccessTimeList;
     private final ArrayList<String> mAttributionTags;
     private final boolean mIsLastUsage;
@@ -99,13 +89,6 @@ public class PermissionHistoryPreference extends Preference {
         mAccessTimeList = accessTimeList;
         mAttributionTags = attributionTags;
         mIsLastUsage = isLastUsage;
-        TypedValue outValue = new TypedValue();
-        mContext.getResources().getValue(R.dimen.permission_access_time_dialog_width_scalar,
-                outValue, true);
-        mDialogWidthScalar = outValue.getFloat();
-        mContext.getResources().getValue(R.dimen.permission_access_time_dialog_height_scalar,
-                outValue, true);
-        mDialogHeightScalar = outValue.getFloat();
         mSessionId = sessionId;
 
         setTitle(mTitle);
@@ -163,58 +146,6 @@ public class PermissionHistoryPreference extends Preference {
         });
     }
 
-    private void setHistoryIcon(ImageView widgetView) {
-        widgetView.setImageDrawable(mWidgetIcon);
-        widgetView.setOnClickListener(v -> {
-            Dialog dialog = new Dialog(mContext);
-            dialog.setContentView(R.layout.access_time_list_dialog);
-
-            ImageView iconView = dialog.findViewById(R.id.icon);
-            iconView.setImageDrawable(mAppIcon);
-
-            TextView titleView = dialog.findViewById(R.id.title);
-            titleView.setText(mTitle);
-
-            TextView subtitleView = dialog.findViewById(R.id.subtitle);
-            subtitleView.setText(mContext.getResources().getString(
-                    R.string.permission_usage_access_dialog_subtitle,
-                    KotlinUtils.INSTANCE.getPermGroupLabel(mContext, mPermissionGroup)));
-
-            RecyclerView recyclerView = dialog.findViewById(R.id.access_time_list);
-            recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-            AccessTimeListAdapter adapter = new AccessTimeListAdapter(mAccessTimeList);
-            recyclerView.setAdapter(adapter);
-
-            if (mIntent != null) {
-                TextView learnMoreView = dialog.findViewById(R.id.learn_more);
-                learnMoreView.setVisibility(View.VISIBLE);
-                learnMoreView.setOnClickListener(v1 -> {
-                    try {
-                        mContext.startActivity(mIntent);
-                    } catch (ActivityNotFoundException e) {
-                        Log.e(LOG_TAG, "No activity found for viewing permission usage.");
-                    }
-                });
-            }
-
-            dialog.show();
-
-            // Resize the dialog.
-            // Have to do this since the default dialog can't be set width and height.
-            DisplayMetrics displayMetrics = new DisplayMetrics();
-            mContext.getDisplay().getMetrics(displayMetrics);
-            int displayWidth = displayMetrics.widthPixels;
-            int displayHeight = displayMetrics.heightPixels;
-            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-            layoutParams.copyFrom(dialog.getWindow().getAttributes());
-            int dialogWindowWidth = (int) (displayWidth * mDialogWidthScalar);
-            int dialogWindowHeight = (int) (displayHeight * mDialogHeightScalar);
-            layoutParams.width = dialogWindowWidth;
-            layoutParams.height = dialogWindowHeight;
-            dialog.getWindow().setAttributes(layoutParams);
-        });
-    }
-
     private void setInfoIcon(ImageView widgetView) {
         if (mIntent != null) {
             widgetView.setImageDrawable(mWidgetIcon);
@@ -257,52 +188,5 @@ public class PermissionHistoryPreference extends Preference {
             return viewUsageIntent;
         }
         return null;
-    }
-
-    /**
-     * Adapter for access time list dialog RecyclerView
-     */
-    public class AccessTimeListAdapter extends
-            RecyclerView.Adapter<AccessTimeListAdapter.ViewHolder> {
-
-        /**
-         * ViewHolder for the AccessTimeListAdapter
-         */
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView accessTimeTextView;
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-
-                accessTimeTextView = itemView.findViewById(R.id.access_time);
-            }
-        }
-
-        private final List<Long> mAccessTimeList;
-
-        public AccessTimeListAdapter(List<Long> accessTimeList) {
-            mAccessTimeList = accessTimeList;
-        }
-
-        @Override
-        public AccessTimeListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            View itemView = inflater.inflate(R.layout.access_time_item, parent, false);
-
-            return new ViewHolder(itemView);
-        }
-
-        @Override
-        public void onBindViewHolder(AccessTimeListAdapter.ViewHolder holder, int position) {
-            Long accessTime = mAccessTimeList.get(position);
-
-            TextView textView = holder.accessTimeTextView;
-            textView.setText(DateFormat.getTimeFormat(mContext).format(accessTime));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mAccessTimeList.size();
-        }
     }
 }
