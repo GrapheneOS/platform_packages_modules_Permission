6352d342b725e7663d87f072a60b213ab7fe8a79
Daniel Micay <danielmicay@gmail.com>
2021-10-17 22:53:22 -0400

    SystemUI: Use new privacy indicators for location
    Android has had location indicators for a while, but let's use the new
    privacy indicator infrastructure for displaying them. This makes them
    integrate better with the new camera and microphone indicators.

--- PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/KotlinUtils.kt
@@ -200,7 +200,7 @@ object KotlinUtils {
             DeviceConfig.getBoolean(
                 DeviceConfig.NAMESPACE_PRIVACY,
                 PROPERTY_LOCATION_INDICATORS_ENABLED,
-                false,
+                true,
             )
     }
 
