41e0a592560cd77438981971979c066b5ebf9723
Karishma Vakil <kvakil@google.com>
2022-02-21 09:32:10 +0000

    [SafetyCenter] Restructure SafetyCenterManager APIs in response to API Council feedback and Refactor test API to override SafetyCenterConfig entirely
    * Rename sendSafetyCenterUpdate -> setSafetySourceData
    * Rename getLastSafetyCenterUpdate -> getSafetySourceData
    * Rename clearSafetyCenterData -> clearAllSafetySourceData
    * Separate out safety source id from SafetySourceData, so it is
      clear what is being "set" in setSafetySourceData and SafetySourceData
      is now simply a data object that can belong to any source.
    * Add an event object while setting data, so Safety Center knows why
      data was set (this was touched upon in API council feedback meeting,
      in particular with receiving data as a result of refresh broadcasts).
    * Other APIs have javadoc changes and order rearrangement (to group
      source and ui APIs) only, no logic changes.
    
    Test: atest CtsSafetyCenterTestCases
    Bug: 219195246
    Bug: 219194447
    Bug: 219078602
    Bug: 218852160
    Bug: 219194453
    Bug: 217944317
    Change-Id: Icdc839c7a35f0c10013a3b4af37db088995c0a40

--- framework-s/java/android/safetycenter/SafetySourceError.java
+++ framework-s/java/android/safetycenter/SafetySourceError.java
@@ -18,17 +18,13 @@ package android.safetycenter;
 
 import static android.os.Build.VERSION_CODES.TIRAMISU;
 
-import android.annotation.IntDef;
 import android.annotation.NonNull;
-import android.annotation.Nullable;
 import android.annotation.SystemApi;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 import androidx.annotation.RequiresApi;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.Objects;
 
 /**
@@ -39,87 +35,38 @@ import java.util.Objects;
 @SystemApi
 @RequiresApi(TIRAMISU)
 public final class SafetySourceError implements Parcelable {
+    @NonNull
+    private final SafetyEvent mSafetyEvent;
 
-    /**
-     * All possible types for a {@link SafetySourceError}.
-     *
-     * @hide
-     */
-    @Retention(RetentionPolicy.SOURCE)
-    @IntDef(prefix = "SOURCE_ERROR_TYPE_", value = {
-            SOURCE_ERROR_TYPE_UNKNOWN,
-            SOURCE_ERROR_TYPE_ACTION_ERROR,
-    })
-    public @interface SourceErrorType {}
-
-    /** Indicates this error is of an unknown type. */
-    public static final int SOURCE_ERROR_TYPE_UNKNOWN = 0;
-
-    /** Indicates this error reports a problem while executing an action on an issue. */
-    public static final int SOURCE_ERROR_TYPE_ACTION_ERROR = 10;
-
-    @SourceErrorType
-    private final int mType;
-    @Nullable
-    private final String mIssueId;
-    @Nullable
-    private final String mActionId;
-
-    private SafetySourceError(
-            @SourceErrorType int type, @Nullable String issueId, @Nullable String actionId) {
-        mType = type;
-        mIssueId = issueId;
-        mActionId = actionId;
+    public SafetySourceError(@NonNull SafetyEvent safetyEvent) {
+        mSafetyEvent = safetyEvent;
     }
 
-    /** Returns the {@link SourceErrorType} of this error. */
-    @SourceErrorType
-    public int getType() {
-        return mType;
+    /** Returns the safety event associated with this error. */
+    @NonNull
+    public SafetyEvent getSafetyEvent() {
+        return mSafetyEvent;
     }
 
-    /**
-     * Returns the id of the {@link SafetySourceIssue} this error is associated with (if any).
-     *
-     * @see SafetySourceIssue#getId()
-     */
-    @Nullable
-    public String getIssueId() {
-        return mIssueId;
-    }
-
-    /**
-     * Returns the id of the {@link SafetySourceIssue.Action} this error is associated with (if
-     * any).
-     *
-     * @see SafetySourceIssue.Action#getId()
-     */
-    @Nullable
-    public String getActionId() {
-        return mActionId;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SafetySourceError that = (SafetySourceError) o;
-        return mType == that.mType
-                && Objects.equals(mIssueId, that.mIssueId)
-                && Objects.equals(mActionId, that.mActionId);
+        return mSafetyEvent.equals(that.mSafetyEvent);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(mType, mIssueId, mActionId);
+        return Objects.hash(mSafetyEvent);
     }
 
     @Override
     public String toString() {
         return "SafetySourceError{"
-                + "mType=" + mType
-                + ", mIssueId='" + mIssueId + '\''
-                + ", mActionId='" + mActionId + '\''
+                + "mSafetyEvent="
+                + mSafetyEvent
                 + '}';
     }
 
@@ -130,19 +77,15 @@ public final class SafetySourceError implements Parcelable {
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
-        dest.writeInt(mType);
-        dest.writeString(mIssueId);
-        dest.writeString(mActionId);
+        dest.writeParcelable(mSafetyEvent, flags);
     }
 
     @NonNull
     public static final Creator<SafetySourceError> CREATOR = new Creator<SafetySourceError>() {
         @Override
         public SafetySourceError createFromParcel(Parcel in) {
-            return new SafetySourceError(
-                    in.readInt(),
-                    in.readString(),
-                    in.readString());
+            return new SafetySourceError(in.readParcelable(SafetyEvent.class.getClassLoader(),
+                    SafetyEvent.class));
         }
 
         @Override
@@ -150,52 +93,4 @@ public final class SafetySourceError implements Parcelable {
             return new SafetySourceError[0];
         }
     };
-
-    /** Builder class for a {@link SafetySourceError}. */
-    public static final class Builder {
-        @SourceErrorType
-        private final int mType;
-        @Nullable
-        private String mIssueId;
-        @Nullable
-        private String mActionId;
-
-        /** Creates a {@link Builder} for a {@link SafetySourceError}. */
-        public Builder(@SourceErrorType int type) {
-            mType = type;
-        }
-
-        /**
-         * Sets the id of the {@link SafetySourceIssue} this error is associated with (if any).
-         *
-         * <p>Typically this would only be used for issues containing actions marked {@link
-         * SafetySourceIssue.Action#isResolving()}, since Safety Center is waiting for a response
-         * and will return an error if none is given in a certain amount of time.
-         */
-        @NonNull
-        public Builder setIssueId(@Nullable String issueId) {
-            mIssueId = issueId;
-            return this;
-        }
-
-        /**
-         * Sets the id of the {@link SafetySourceIssue.Action} this error is associated with (if
-         * any).
-         *
-         * <p>Typically this would only be used for actions marked {@link
-         * SafetySourceIssue.Action#isResolving()}, since Safety Center is waiting for a response
-         * and will return an error if none is given in a certain amount of time.
-         */
-        @NonNull
-        public Builder setActionId(@Nullable String actionId) {
-            mActionId = actionId;
-            return this;
-        }
-
-        /** Creates the {@link SafetySourceError} defined by this {@link Builder}. */
-        @NonNull
-        public SafetySourceError build() {
-            return new SafetySourceError(mType, mIssueId, mActionId);
-        }
-    }
 }
