a6aa3ce80ca56961bf3545394f71eee9f1cc1f23
Prabal Singh <prabalsingh@google.com>
2022-05-04 17:26:17 +0100

    WorkPolicyInfo: fix pendingIntent options
    Pending intent needs to have the correct context for managed device
    owner and managed profile owner. Also need to set the correct package
    name for the Intent
    
    Bug: b/231337542
    Test: atest WorkPolicyInfoTest
    Change-Id: I69986fec7ffe8be85d99234ea85ff0e64b903763

--- PermissionController/tests/mocking/src/com/android/permissioncontroller/tests/mocking/privacysources/WorkPolicyInfoTest.kt
+++ PermissionController/tests/mocking/src/com/android/permissioncontroller/tests/mocking/privacysources/WorkPolicyInfoTest.kt
@@ -20,7 +20,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.provider.Settings
 import android.safetycenter.SafetyCenterManager
 import android.safetycenter.SafetyEvent
 import android.safetycenter.SafetySourceData
@@ -80,9 +79,6 @@ class WorkPolicyInfoTest {
         whenever(application.applicationContext).thenReturn(application)
         whenever(application.getSystemService(SafetyCenterManager::class.java))
             .thenReturn(mockSafetyCenterManager)
-        // Default state for hasWorkPolicy for tests.
-        // For tests where hasWorkPolicy needs to be false, do it in the test logic
-        whenever(mockWorkPolicyUtils.hasWorkPolicy()).thenReturn(true)
         workPolicyInfo = WorkPolicyInfo(mockWorkPolicyUtils)
     }
 
@@ -93,14 +89,19 @@ class WorkPolicyInfoTest {
 
     @Test
     fun safetyCenterEnabledChanged_safetyCenterEnabled() {
+        val intent =
+            Intent(Intent.ACTION_BOOT_COMPLETED)
+                .putExtra(
+                    SafetyCenterManager.EXTRA_REFRESH_SAFETY_SOURCES_BROADCAST_ID,
+                    SafetyCenterManager.EXTRA_REFRESH_SAFETY_SOURCES_BROADCAST_ID)
+
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentDO).thenReturn(intent)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentPO).thenReturn(null)
+
         workPolicyInfo.safetyCenterEnabledChanged(context, true)
 
         val pendingIntent =
-            PendingIntent.getActivity(
-                context,
-                0,
-                Intent(Settings.ACTION_SHOW_WORK_POLICY_INFO),
-                PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val expectedSafetySourceStatus: SafetySourceStatus =
             SafetySourceStatus.Builder(
                     context.getText(R.string.work_policy_title),
@@ -154,17 +155,16 @@ class WorkPolicyInfoTest {
     }
 
     @Test
-    fun rescanAndPushSafetyCenterData_eventRebooted() {
+    fun rescanAndPushSafetyCenterData_eventRebooted_deviceOwner() {
         val intent = Intent(Intent.ACTION_BOOT_COMPLETED)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentDO).thenReturn(intent)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentPO).thenReturn(null)
+
         workPolicyInfo.rescanAndPushSafetyCenterData(
             context, intent, RefreshEvent.EVENT_DEVICE_REBOOTED)
 
         val pendingIntent =
-            PendingIntent.getActivity(
-                context,
-                0,
-                Intent(Settings.ACTION_SHOW_WORK_POLICY_INFO),
-                PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val expectedSafetySourceStatus: SafetySourceStatus =
             SafetySourceStatus.Builder(
                     context.getText(R.string.work_policy_title),
@@ -186,22 +186,21 @@ class WorkPolicyInfoTest {
     }
 
     @Test
-    fun rescanAndPushSafetyCenterData_eventRefresh() {
+    fun rescanAndPushSafetyCenterData_eventRefresh_deviceOwner() {
         val intent =
             Intent(Intent.ACTION_BOOT_COMPLETED)
                 .putExtra(
                     SafetyCenterManager.EXTRA_REFRESH_SAFETY_SOURCES_BROADCAST_ID,
                     SafetyCenterManager.EXTRA_REFRESH_SAFETY_SOURCES_BROADCAST_ID)
 
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentDO).thenReturn(intent)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentPO).thenReturn(null)
+
         workPolicyInfo.rescanAndPushSafetyCenterData(
             context, intent, RefreshEvent.EVENT_REFRESH_REQUESTED)
 
         val pendingIntent =
-            PendingIntent.getActivity(
-                context,
-                0,
-                Intent(Settings.ACTION_SHOW_WORK_POLICY_INFO),
-                PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val expectedSafetySourceStatus: SafetySourceStatus =
             SafetySourceStatus.Builder(
                     context.getText(R.string.work_policy_title),
@@ -228,14 +227,14 @@ class WorkPolicyInfoTest {
     }
 
     @Test
-    fun rescanAndPushSafetyCenterData_eventUnknown() {
+    fun rescanAndPushSafetyCenterData_eventUnknown_profileOwner() {
         val intent = Intent(Intent.ACTION_BOOT_COMPLETED)
+
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentDO).thenReturn(null)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentPO).thenReturn(intent)
+
         val pendingIntent =
-            PendingIntent.getActivity(
-                context,
-                0,
-                Intent(Settings.ACTION_SHOW_WORK_POLICY_INFO),
-                PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
         workPolicyInfo.rescanAndPushSafetyCenterData(context, intent, RefreshEvent.UNKNOWN)
 
@@ -261,7 +260,8 @@ class WorkPolicyInfoTest {
 
     @Test
     fun rescanAndPushSafetyCenterData_hasWorkPolicyFalse() {
-        whenever(mockWorkPolicyUtils.hasWorkPolicy()).thenReturn(false)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentPO).thenReturn(null)
+        whenever(mockWorkPolicyUtils.workPolicyInfoIntentDO).thenReturn(null)
 
         val intent = Intent(Intent.ACTION_BOOT_COMPLETED)
         workPolicyInfo.rescanAndPushSafetyCenterData(context, intent, RefreshEvent.UNKNOWN)
