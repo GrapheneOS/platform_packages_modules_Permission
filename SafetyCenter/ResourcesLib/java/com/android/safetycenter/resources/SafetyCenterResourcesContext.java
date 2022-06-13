953d97e2cc9e27e43534b9e86bc9cd56c2528998
Yuri Ufimtsev <yufimtsev@google.com>
2022-06-13 09:19:32 +0000

    Enable Search indexing for SafetyCenter entries
    Test: manual
    
    Bug: 227149388
    Bug: 227149787
    Change-Id: I5ce5c0f5a9042de5fc37c04802fa490fa5b1e011

--- SafetyCenter/ResourcesLib/java/com/android/safetycenter/resources/SafetyCenterResourcesContext.java
+++ SafetyCenter/ResourcesLib/java/com/android/safetycenter/resources/SafetyCenterResourcesContext.java
@@ -18,8 +18,6 @@ package com.android.safetycenter.resources;
 
 import static java.util.Objects.requireNonNull;
 
-import android.annotation.NonNull;
-import android.annotation.Nullable;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -29,7 +27,9 @@ import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.util.Log;
 
-import com.android.internal.annotations.VisibleForTesting;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import java.io.File;
 import java.io.InputStream;
