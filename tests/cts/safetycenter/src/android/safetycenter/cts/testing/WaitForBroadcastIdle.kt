69a90c8b97c08a6613bf046f90aa08c282670565
Elliot Sisteron <elliotsisteron@google.com>
2022-06-27 11:53:11 +0000

    Wait for broadcast idle before running tests.
    That should avoid us having to timeout this call, since the problem
    seems to be that BOOT_COMPLETED broadcasts are being dispatched prior to
    running our tests which then causes `waitForBroadcastIdle()` to run for
    too long.
    
    Hopefully the RunCommandTargetPreparer is allowed to run for long
    enough, which should ensure all further calls to `waitForBroadcastIdle()` complete on time.
    
    Bug: 237104364
    Test: atest CtsSafetyCenterTestCases
    Change-Id: I4a57c5892a7835eea4e1da0d5bb5f6eb36678ea1

--- tests/cts/safetycenter/src/android/safetycenter/cts/testing/WaitForBroadcastIdle.kt
+++ tests/cts/safetycenter/src/android/safetycenter/cts/testing/WaitForBroadcastIdle.kt
@@ -19,32 +19,14 @@ package android.safetycenter.cts.testing
 import android.Manifest.permission.DUMP
 import android.app.ActivityManager
 import android.content.Context
-import android.safetycenter.cts.testing.Coroutines.runBlockingWithTimeout
 import android.safetycenter.cts.testing.ShellPermissions.callWithShellPermissionIdentity
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.runInterruptible
 
 /** A class that allows waiting for the broadcast queue to be idle. */
 object WaitForBroadcastIdle {
 
     /** Waits for the broadcast queue to be idle. */
     fun Context.waitForBroadcastIdle() {
-        try {
-            callWithShellPermissionIdentity(
-                { runBlockingWithTimeout { waitForBroadcastIdleAsync() } }, DUMP)
-        } catch (ex: TimeoutCancellationException) {
-            Log.e(TAG, "Timeout while waiting for broadcast queue to be idle")
-        }
-    }
-
-    // This method is here to be able to timeout the `waitForBroadcastIdle` call, since the timeout
-    // cannot apply to blocking code.
-    private suspend fun Context.waitForBroadcastIdleAsync() {
         val activityManager = getSystemService(ActivityManager::class.java)!!
-        runInterruptible(Dispatchers.IO) { activityManager.waitForBroadcastIdle() }
+        callWithShellPermissionIdentity({ activityManager.waitForBroadcastIdle() }, DUMP)
     }
-
-    private const val TAG = "WaitForBroadcastIdle"
 }
