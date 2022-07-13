fb172fe40d5282eeba77bf1adadf8f76552f12d3
Elliot Sisteron <elliotsisteron@google.com>
2022-07-13 13:40:24 +0000

    Add extra check to override IconAction.
    We can check the Intent action and request code that way: since the
    issue is that the PendingIntent is cached, then if we recreate it
    ourselves with request code 0 it will return the "wrong" instance that
    doesn't have the right extras.
    
    Bug: 203098016
    Test: atest CtsSafetyCenterTestCases
    Change-Id: I0965768cf3c3c1a1f30534906b194f29167a8a63

--- service/java/com/android/safetycenter/SafetyCenterDataTracker.java
+++ service/java/com/android/safetycenter/SafetyCenterDataTracker.java
@@ -1364,11 +1364,17 @@ final class SafetyCenterDataTracker {
 
         // TODO(b/222838784): Validate that the intent action is available.
 
+        return toPendingIntent(context, 0, new Intent(intentAction));
+    }
+
+    @NonNull
+    private static PendingIntent toPendingIntent(
+            @NonNull Context packageContext, int requestCode, @NonNull Intent intent) {
         // This call is required for getIntentSender() to be allowed to send as another package.
         final long identity = Binder.clearCallingIdentity();
         try {
             return PendingIntent.getActivity(
-                    context, 0, new Intent(intentAction), PendingIntent.FLAG_IMMUTABLE);
+                    packageContext, requestCode, intent, PendingIntent.FLAG_IMMUTABLE);
         } finally {
             Binder.restoreCallingIdentity(identity);
         }
@@ -1394,23 +1400,11 @@ final class SafetyCenterDataTracker {
         if (!SafetyCenterFlags.getReplaceLockScreenIconAction()) {
             return pendingIntent;
         }
-        String packageName = pendingIntent.getCreatorPackage();
-        UserHandle userHandle = pendingIntent.getCreatorUserHandle();
-        PendingIntent pendingIntentOverride =
-                createLockScreenIconActionPendingIntentOverride(
-                        packageName, userHandle.getIdentifier());
-        if (pendingIntentOverride == null) {
-            return pendingIntent;
-        }
-        return pendingIntentOverride;
-    }
-
-    @Nullable
-    private PendingIntent createLockScreenIconActionPendingIntentOverride(
-            @NonNull String settingsPackageName, @UserIdInt int userId) {
+        String settingsPackageName = pendingIntent.getCreatorPackage();
+        int userId = pendingIntent.getCreatorUserHandle().getIdentifier();
         Context packageContext = toPackageContextAsUser(settingsPackageName, userId);
         if (packageContext == null) {
-            return null;
+            return pendingIntent;
         }
         Resources settingsResources = packageContext.getResources();
         int hasSettingsFixedIssueResourceId =
@@ -1422,7 +1416,7 @@ final class SafetyCenterDataTracker {
             boolean hasSettingsFixedIssue =
                     settingsResources.getBoolean(hasSettingsFixedIssueResourceId);
             if (hasSettingsFixedIssue) {
-                return null;
+                return pendingIntent;
             }
         }
         Intent intent =
@@ -1435,16 +1429,17 @@ final class SafetyCenterDataTracker {
                                 settingsPackageName + ".security.screenlock.ScreenLockSettings")
                         .putExtra(":settings:source_metrics", 1917)
                         .putExtra("page_transition_type", 0);
-        final long callingId = Binder.clearCallingIdentity();
-        try {
-            return PendingIntent.getActivity(
-                    packageContext,
-                    ANDROID_LOCK_SCREEN_ICON_ACTION_REQ_CODE,
-                    intent,
-                    PendingIntent.FLAG_IMMUTABLE);
-        } finally {
-            Binder.restoreCallingIdentity(callingId);
+        PendingIntent offendingPendingIntent = toPendingIntent(packageContext, 0, intent);
+        if (!offendingPendingIntent.equals(pendingIntent)) {
+            return pendingIntent;
         }
+        // If creating that PendingIntent with request code 0 returns the same value as the
+        // PendingIntent that was sent to Safety Center, then we’re most likely hitting the caching
+        // issue described in this method’s documentation.
+        // i.e. the intent action and component of the cached PendingIntent are the same, but the
+        // extras are actually different so we should ensure we create a brand new PendingIntent by
+        // changing the request code.
+        return toPendingIntent(packageContext, ANDROID_LOCK_SCREEN_ICON_ACTION_REQ_CODE, intent);
     }
 
     @Nullable
