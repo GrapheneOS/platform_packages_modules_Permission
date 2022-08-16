33d0db3848e1cfcbc02091169510cea3cd87f3af
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-16 16:20:26 +0300

    enable usage timeline for all permission groups
    Original author is Danny Lin <danny@kdrag0n.dev>
    
    Ported from 12: 9ffdad9c33f8b8b2195482cb47e4844c487a771f

--- PermissionController/src/com/android/permissioncontroller/permission/ui/model/v31/PermissionUsageControlPreferenceUtils.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/model/v31/PermissionUsageControlPreferenceUtils.kt
@@ -39,7 +39,16 @@ object PermissionUsageControlPreferenceUtils {
     private val SENSOR_DATA_PERMISSIONS: List<String> = listOf(
         Manifest.permission_group.LOCATION,
         Manifest.permission_group.CAMERA,
-        Manifest.permission_group.MICROPHONE
+        Manifest.permission_group.MICROPHONE,
+        Manifest.permission_group.SENSORS,
+        Manifest.permission_group.CALENDAR,
+        Manifest.permission_group.CALL_LOG,
+        Manifest.permission_group.CONTACTS,
+        Manifest.permission_group.STORAGE,
+        Manifest.permission_group.NEARBY_DEVICES,
+        Manifest.permission_group.PHONE,
+        Manifest.permission_group.ACTIVITY_RECOGNITION,
+        Manifest.permission_group.SMS
     )
 
     @JvmStatic
@@ -105,7 +114,7 @@ object PermissionUsageControlPreferenceUtils {
             Manifest.permission_group.MICROPHONE -> {
                 PERMISSION_USAGE_FRAGMENT_INTERACTION__ACTION__MICROPHONE_ACCESS_TIMELINE_VIEWED
             }
-            else -> 0
+            else -> return
         }
         PermissionControllerStatsLog.write(PERMISSION_USAGE_FRAGMENT_INTERACTION, sessionId, act)
     }
