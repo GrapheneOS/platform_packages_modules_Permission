482df4beebf456a4c487ab75abc66a41cb4cfde0
Jay Thomas Sullivan <jaysullivan@google.com>
2022-09-29 19:44:26 -0700

    Migrate STORAGE permissions to READ_MEDIA on T
    On Android platform T, when the platform was upgraded, the
    READ_MEDIA_* permissions were not always getting auto-granted.
    
    Specifically: when an app has READ_EXTERNAL_STORAGE granted, but also
    has {READ_MEDIA_IMAGES and/or READ_MEDIA_VIDEO and/or READ_MEDIA_AUDIO}
    in its manifest at the time of the platform upgrade. When the platform
    upgrade occurs: whichever of the READ_MEDIA_ permissions are in the
    manifest will not be granted, and vice versa (whichever of the
    READ_MEDIA_ permissions are not in the manifest will be granted.)
    
    But, if these permissions are in the manifest, then we want them to be
    auto-granted if (and only if) the STORAGE permissions
    (READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE) were granted.
    
    The solution used here is to perform a one-time upgrade patch which does
    the following (upon upgrading to the T platform, for all apps):
    
    1. If the user has previously granted the app  READ_EXTERNAL_STORAGE or
       WRITE_EXTERNAL_STORAGE, then grant all READ_MEDIA_* permissions which
       are requested in the manifest
    2. Regardless of #1, revoke READ_EXTERNAL_STORAGE and
       WRITE_EXTERNAL_STORAGE
    
    Fixes: 249702391
    Test: atest RuntimePermissionsUpgradeControllerTest
    Change-Id: Ieb048f5d38331141bb96b2a32d94e81233a5465e

--- PermissionController/src/com/android/permissioncontroller/permission/model/livedatatypes/LightAppPermGroup.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/model/livedatatypes/LightAppPermGroup.kt
@@ -42,8 +42,11 @@ data class LightAppPermGroup(
     val hasInstallToRuntimeSplit: Boolean,
     val specialLocationGrant: Boolean?
 ) {
-    constructor(pI: LightPackageInfo, pGI: LightPermGroupInfo, perms: Map<String, LightPermission>):
-        this(pI, pGI, perms, false, null)
+    constructor(
+        pI: LightPackageInfo,
+        pGI: LightPermGroupInfo,
+        perms: Map<String, LightPermission>
+    ) : this(pI, pGI, perms, false, null)
 
     /**
      * All unrestricted permissions. Usually restricted permissions are ignored
@@ -162,6 +165,16 @@ data class LightAppPermGroup(
      * Whether any permissions in this group are revoke-when-requested
      */
     val isRevokeWhenRequested = permissions.any { it.value.isRevokeWhenRequested }
+
+    /**
+     * Whether any of this App Permission Groups permissions are fixed by the user
+     */
+    val isUserFixed = foreground.isUserFixed || background.isUserFixed
+
+    /**
+     * Whether any of this App Permission Group's permissions are set by the user
+     */
+    val isUserSet = foreground.isUserSet || background.isUserSet
 
     /**
      * A subset of the AppPermissionGroup, representing either the background or foreground permissions
