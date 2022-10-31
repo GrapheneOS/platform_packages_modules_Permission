35a5024f540f9ad69e1e4d97edf8db5ad655b569
Shriya Gupta <shrigupt@google.com>
2022-10-31 18:39:33 +0000

    Add intent extra to SafetyCenterManager API for opening subpages
    This CL contains first part of the API changes described in go/angel-u-subpages. It also contains minimal UI additions required to construct the CTS test. Implementation of this UI will be completed in later CLs.
    
    Test: atest SafetyCenterSubpagesTest
    Bug: 255495618
    Change-Id: I027f430ac5b1965930c281ca4e1b6aa3946f5152

---
+++ PermissionController/src/com/android/permissioncontroller/safetycenter/ui/SafetyCenterSubpageFragment.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.safetycenter.ui
+
+import android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+import android.os.Bundle
+import android.safetycenter.SafetyCenterEntryGroup
+import android.safetycenter.SafetyCenterEntryOrGroup
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceFragmentCompat
+import com.android.permissioncontroller.R
+import com.android.permissioncontroller.safetycenter.ui.model.LiveSafetyCenterViewModelFactory
+import com.android.permissioncontroller.safetycenter.ui.model.SafetyCenterUiData
+import com.android.permissioncontroller.safetycenter.ui.model.SafetyCenterViewModel
+import com.android.safetycenter.internaldata.SafetyCenterIds
+
+/** A fragment that represents a generic subpage in Safety Center. */
+@RequiresApi(UPSIDE_DOWN_CAKE)
+class SafetyCenterSubpageFragment(private val sourceGroupId: String) : PreferenceFragmentCompat() {
+
+    private lateinit var viewModel: SafetyCenterViewModel
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.safety_center_subpage, rootKey)
+
+        viewModel =
+            ViewModelProvider(
+                    requireActivity(),
+                    LiveSafetyCenterViewModelFactory(requireActivity().getApplication()))
+                .get(SafetyCenterViewModel::class.java)
+        viewModel.safetyCenterUiLiveData.observe(
+            this,
+            Observer { uiData: SafetyCenterUiData? -> this.renderSafetyCenterEntryGroup(uiData) })
+    }
+
+    private fun renderSafetyCenterEntryGroup(uiData: SafetyCenterUiData?) {
+        val entryGroup = getMatchingGroup(uiData, sourceGroupId)
+        if (entryGroup == null) {
+            Log.w(TAG, "$sourceGroupId doesn't match any of the existing SafetySourcesGroup IDs")
+            requireActivity().getFragmentManager().popBackStack()
+        }
+        requireActivity().setTitle(entryGroup!!.title)
+    }
+
+    private fun getMatchingGroup(
+        uiData: SafetyCenterUiData?,
+        sourceGroupId: String
+    ): SafetyCenterEntryGroup? {
+        val entryOrGroups: List<SafetyCenterEntryOrGroup>? =
+            uiData?.safetyCenterData?.entriesOrGroups
+        val entryGroups = entryOrGroups?.mapNotNull { it.entryGroup }
+        return entryGroups?.find {
+            SafetyCenterIds.entryGroupIdFromString(it.id).getSafetySourcesGroupId() == sourceGroupId
+        }
+    }
+
+    companion object {
+        private const val TAG: String = "SafetyCenterSubpageFragment"
+    }
+}
---
+++ tests/cts/safetycenter/src/android/safetycenter/cts/ui/SafetyCenterSubpagesTest.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.safetycenter.cts.ui
+
+import android.content.Context
+import android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+import android.os.Bundle
+import android.safetycenter.SafetyCenterManager.EXTRA_SAFETY_SOURCES_GROUP_ID
+import android.safetycenter.cts.testing.SafetyCenterActivityLauncher.launchSafetyCenterActivity
+import android.safetycenter.cts.testing.SafetyCenterCtsConfigs.MULTIPLE_SOURCES_GROUP_ID_1
+import android.safetycenter.cts.testing.SafetyCenterCtsConfigs.MULTIPLE_SOURCE_GROUPS_CONFIG
+import android.safetycenter.cts.testing.SafetyCenterCtsHelper
+import android.safetycenter.cts.testing.SafetyCenterFlags
+import android.safetycenter.cts.testing.SafetyCenterFlags.deviceSupportsSafetyCenter
+import android.safetycenter.cts.testing.UiTestHelper.waitDisplayed
+import android.safetycenter.cts.testing.UiTestHelper.waitNotDisplayed
+import android.support.test.uiautomator.By
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import org.junit.After
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/** CTS tests for generic subpages in Safety Center. */
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = UPSIDE_DOWN_CAKE, codeName = "UpsideDownCake")
+class SafetyCenterSubpagesTest {
+    private val context: Context = getApplicationContext()
+    private val safetyCenterCtsHelper = SafetyCenterCtsHelper(context)
+
+    // JUnit's Assume is not supported in @BeforeClass by the CTS tests runner, so this is used to
+    // manually skip the setup and teardown methods.
+    private val shouldRunTests = context.deviceSupportsSafetyCenter()
+
+    @Before
+    fun assumeDeviceSupportsSafetyCenterToRunTests() {
+        assumeTrue(shouldRunTests)
+    }
+
+    @Before
+    fun enableSafetyCenterBeforeTest() {
+        if (!shouldRunTests) {
+            return
+        }
+        safetyCenterCtsHelper.setup()
+        SafetyCenterFlags.showSubpages = true
+    }
+
+    @After
+    fun clearDataAfterTest() {
+        if (!shouldRunTests) {
+            return
+        }
+        safetyCenterCtsHelper.reset()
+    }
+
+    @Test
+    fun launchSafetyCenter_withSubpagesIntentExtra_showsSubpageTitle() {
+        safetyCenterCtsHelper.setConfig(MULTIPLE_SOURCE_GROUPS_CONFIG)
+        val extras = Bundle()
+        extras.putString(EXTRA_SAFETY_SOURCES_GROUP_ID, MULTIPLE_SOURCES_GROUP_ID_1)
+
+        context.launchSafetyCenterActivity(extras) {
+            // CollapsingToolbar title can't be found by text, so using description instead.
+            waitDisplayed(
+                By.desc(
+                    context.getString(
+                        MULTIPLE_SOURCE_GROUPS_CONFIG.safetySourcesGroups.first()!!.titleResId)))
+        }
+    }
+
+    @Test
+    fun launchSafetyCenter_withSubpagesFlagDisabled_showsHomepageTitle() {
+        SafetyCenterFlags.showSubpages = false
+        safetyCenterCtsHelper.setConfig(MULTIPLE_SOURCE_GROUPS_CONFIG)
+        val extras = Bundle()
+        extras.putString(EXTRA_SAFETY_SOURCES_GROUP_ID, MULTIPLE_SOURCES_GROUP_ID_1)
+
+        context.launchSafetyCenterActivity(extras) {
+            // CollapsingToolbar title can't be found by text, so using description instead.
+            waitDisplayed(By.desc("Security & privacy"))
+        }
+    }
+
+    @Test
+    fun launchSafetyCenter_withNonExistingGroupID_displaysNothing() {
+        safetyCenterCtsHelper.setConfig(MULTIPLE_SOURCE_GROUPS_CONFIG)
+        val extras = Bundle()
+        extras.putString(EXTRA_SAFETY_SOURCES_GROUP_ID, "non_existing_group_id")
+
+        context.launchSafetyCenterActivity(extras) {
+            waitNotDisplayed(
+                By.desc(
+                    context.getString(
+                        MULTIPLE_SOURCE_GROUPS_CONFIG.safetySourcesGroups.first()!!.titleResId)))
+        }
+    }
+}
