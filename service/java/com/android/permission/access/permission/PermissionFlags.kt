957bdab609e52a8ee8939480cd45eb30ed8bf5d2
Yi-an Chen <theianchen@google.com>
2022-11-15 22:04:23 +0000

    Add PermissionPolicy implementation for split perms
    Bug: 182523293
    Test: build
    Change-Id: I2fe5a96163e8d0281caa3e7b7caac55ee5e00b2d

--- service/java/com/android/permission/access/permission/PermissionFlags.kt
+++ service/java/com/android/permission/access/permission/PermissionFlags.kt
@@ -20,8 +20,13 @@ object PermissionFlags {
     const val INSTALL_GRANTED = 1 shl 0
     const val INSTALL_REVOKED = 1 shl 1
     const val PROTECTION_GRANTED = 1 shl 2
-    const val API_GRANTED = 1 shl 3
-    const val ROLE_GRANTED = 1 shl 4
+    const val ROLE_GRANTED = 1 shl 3
+    // For permissions that are granted in other ways,
+    // ex: via an API or implicit permissions that inherit from granted install permissions
+    const val OTHER_GRANTED = 1 shl 4
+    // For the permissions that are implicit for the package
+    const val IMPLICIT = 1 shl 5
 
-    const val MASK_ALL = 0.inv()
+    const val MASK_GRANTED = INSTALL_GRANTED or PROTECTION_GRANTED or OTHER_GRANTED or ROLE_GRANTED
+    const val MASK_RUNTIME = OTHER_GRANTED or IMPLICIT
 }
