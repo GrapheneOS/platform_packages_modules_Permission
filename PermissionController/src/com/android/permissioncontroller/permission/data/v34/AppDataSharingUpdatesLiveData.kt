98835b900dbb5680f48f2d46686c0dbd1dff4327
Karishma Vakil <kvakil@google.com>
2023-01-07 02:52:05 +0000

    [Safety Labels] Load app safety labels from disk to display in AppDataSharingUpdatesPage
    Bug: 261665490
    Bug: 263838803
    Bug: 263838456
    Test: atest CtsPermission3TestCases:AppDataSharingUpdatesTest
    Change-Id: I79dde88744b53eb8cab470de7639241dd544d6ea

--- PermissionController/src/com/android/permissioncontroller/permission/data/v34/AppDataSharingUpdatesLiveData.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/data/v34/AppDataSharingUpdatesLiveData.kt
@@ -18,13 +18,19 @@ package com.android.permissioncontroller.permission.data.v34
 
 import android.app.Application
 import android.os.Build
-import android.provider.DeviceConfig
-import android.provider.DeviceConfig.NAMESPACE_PRIVACY
 import androidx.annotation.RequiresApi
 import com.android.permissioncontroller.permission.data.SmartAsyncMediatorLiveData
 import com.android.permissioncontroller.permission.model.v34.AppDataSharingUpdate
 import com.android.permissioncontroller.permission.model.v34.AppDataSharingUpdate.Companion.LOCATION_CATEGORY
-import com.android.permissioncontroller.permission.model.v34.DataSharingUpdateType
+import com.android.permissioncontroller.permission.model.v34.AppDataSharingUpdate.Companion.buildUpdateIfSignificantChange
+import com.android.permissioncontroller.safetylabel.AppsSafetyLabelHistory
+import com.android.permissioncontroller.safetylabel.AppsSafetyLabelHistory.DataCategory
+import com.android.permissioncontroller.safetylabel.AppsSafetyLabelHistory.DataLabel
+import com.android.permissioncontroller.safetylabel.AppsSafetyLabelHistory.SafetyLabel
+import com.android.permissioncontroller.safetylabel.AppsSafetyLabelHistoryPersistence
+import java.time.Instant
+import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.Job
 
 /** LiveData for [AppDataSharingUpdate]s. */
@@ -33,34 +39,51 @@ class AppDataSharingUpdatesLiveData(val app: Application) :
     SmartAsyncMediatorLiveData<List<AppDataSharingUpdate>>() {
 
     override suspend fun loadDataAndPostValue(job: Job) {
-        // TODO(b/261665490): Read updates from AppSafetyLabelHistoryPersistence instead.
-        if (!DeviceConfig.getBoolean(
-            NAMESPACE_PRIVACY, PLACEHOLDER_SAFETY_LABEL_UPDATES_FLAG, false)) {
-            postValue(listOf())
-            return
-        }
+        // TODO(b/261660881): This code serves to enable testing business logic in CTS. Remove when
+        //  package add broadcasts can invoke writing of safety labels to persistence.
+        writeTestSafetyLabelsToPersistence()
 
-        val placeholderUpdates =
-            listOf(
-                AppDataSharingUpdate(
-                    PLACEHOLDER_PACKAGE_NAME_1,
-                    mapOf(
-                        LOCATION_CATEGORY to
-                            DataSharingUpdateType.ADDS_SHARING_WITHOUT_ADVERTISING_PURPOSE)),
-                AppDataSharingUpdate(
-                    PLACEHOLDER_PACKAGE_NAME_2,
-                    mapOf(
-                        LOCATION_CATEGORY to
-                            DataSharingUpdateType.ADDS_SHARING_WITH_ADVERTISING_PURPOSE)))
+        val appSafetyLabelDiffsFromPersistence =
+            AppsSafetyLabelHistoryPersistence.getAppSafetyLabelDiffs(
+                Instant.now()
+                    .minusSeconds(TimeUnit.DAYS.toSeconds(DATA_SHARING_UPDATE_PERIOD_DAYS)),
+                AppsSafetyLabelHistoryPersistence.getSafetyLabelHistoryFile(app.applicationContext))
+        val updatesFromPersistence =
+            appSafetyLabelDiffsFromPersistence.mapNotNull { it.buildUpdateIfSignificantChange() }
 
-        postValue(placeholderUpdates)
+        postValue(updatesFromPersistence)
+    }
+
+    /** Writes safety labels for a test app to safety label history. */
+    private fun writeTestSafetyLabelsToPersistence() {
+        val historyFile =
+            AppsSafetyLabelHistoryPersistence.getSafetyLabelHistoryFile(app.applicationContext)
+        AppsSafetyLabelHistoryPersistence.clear(historyFile)
+        AppsSafetyLabelHistoryPersistence.recordSafetyLabel(
+            SAFETY_LABEL_TEST_PACKAGE_V1, historyFile)
+        AppsSafetyLabelHistoryPersistence.recordSafetyLabel(
+            SAFETY_LABEL_TEST_PACKAGE_V2, historyFile)
     }
 
     /** Companion object for [AppDataSharingUpdatesLiveData]. */
     companion object {
         private const val PLACEHOLDER_PACKAGE_NAME_1 = "com.android.systemui"
         private const val PLACEHOLDER_PACKAGE_NAME_2 = "com.android.bluetooth"
+        private const val TEST_PACKAGE_NAME = "android.permission3.cts.usepermission"
         private const val PLACEHOLDER_SAFETY_LABEL_UPDATES_FLAG =
             "placeholder_safety_label_updates_flag"
+        private const val DATA_SHARING_UPDATE_PERIOD_DAYS: Long = 30
+
+        private val SAFETY_LABEL_TEST_PACKAGE_V1 =
+            SafetyLabel(
+                AppsSafetyLabelHistory.AppInfo(TEST_PACKAGE_NAME),
+                ZonedDateTime.now().minusDays(5).toInstant(),
+                DataLabel(mapOf(LOCATION_CATEGORY to DataCategory(false))))
+
+        private val SAFETY_LABEL_TEST_PACKAGE_V2 =
+            SafetyLabel(
+                AppsSafetyLabelHistory.AppInfo(TEST_PACKAGE_NAME),
+                ZonedDateTime.now().minusDays(2).toInstant(),
+                DataLabel(mapOf(LOCATION_CATEGORY to DataCategory(true))))
     }
 }
