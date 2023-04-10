08c6c7c2af769362d81c2f12ee3b423871d0cebe
Yi-an Chen <theianchen@google.com>
2023-04-10 21:49:17 +0000

    Add tests for PermissionMapping
    Bug: 274702991
    
    Notes for R coverage calculation:
    This change removes 16 lines and add 8 lines of coverage.
    Together with the 37 lines of exemption from init {} and
    addHealthPermissionsToPlatform,
    it closes 61 lines of gap.
    
    Test: PermissionMappingTest
    Change-Id: Ibde6cf4149481631539a8a3de4cecab86ab6338a

---
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/v34/SafetyLabelUtils.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.permission.utils.v34
+
+import com.android.permission.safetylabel.DataCategory
+import com.android.permission.safetylabel.DataType
+import com.android.permission.safetylabel.DataTypeConstants
+import com.android.permission.safetylabel.SafetyLabel
+import com.android.permissioncontroller.permission.utils.PermissionMapping
+
+object SafetyLabelUtils {
+    /*
+     * Get the sharing purposes for a SafetyLabel related to a specific permission group.
+     */
+    @JvmStatic
+    fun getSafetyLabelSharingPurposesForGroup(
+        safetyLabel: SafetyLabel,
+        groupName: String
+    ): Set<Int> {
+        val purposeSet = mutableSetOf<Int>()
+        val categoriesForPermission = PermissionMapping
+            .getDataCategoriesForPermissionGroup(groupName)
+        categoriesForPermission.forEach categoryLoop@{ category ->
+            val dataCategory: DataCategory? = safetyLabel.dataLabel.dataShared[category]
+            if (dataCategory == null) {
+                // Continue to next
+                return@categoryLoop
+            }
+            val typesForCategory = DataTypeConstants.getValidDataTypesForCategory(category)
+            typesForCategory.forEach typeLoop@{ type ->
+                val dataType: DataType? = dataCategory.dataTypes[type]
+                if (dataType == null) {
+                    // Continue to next
+                    return@typeLoop
+                }
+                if (dataType.purposeSet.isNotEmpty()) {
+                    purposeSet.addAll(dataType.purposeSet)
+                }
+            }
+        }
+
+        return purposeSet
+    }
+}
