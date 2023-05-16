fe9905f8e3c214b60d070e8da3f36a791456b47f
Nate Myren <ntmyren@google.com>
2023-05-16 15:12:40 -0700

    Refactor GrantPermissions, introduce GrantBehaviors
    Refactor the GrantPermissionsViewModel. Move any group specific behavior
    to a list of GrantBehaviors, which determine if a request is considered
    granted, if it's considered fixed, and determines what type of prompt
    and deny behavior should show. These prompts are enumerated explicitly,
    and should be much more human understandable.
    
    Bug: 284183336
    Test: atest CtsPermission3TestCases
    Change-Id: I29c4ee20ecec4d89b485ffeedeea52adfb573ae1

---
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/model/grantPermissions/NotificationGrantBehavior.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.permission.ui.model.grantPermissions
+
+import android.os.Build.VERSION_CODES.TIRAMISU
+import com.android.permissioncontroller.permission.model.livedatatypes.LightAppPermGroup
+import com.android.permissioncontroller.permission.ui.model.DenyButton
+import com.android.permissioncontroller.permission.ui.model.Prompt
+
+/**
+ * The Notification permission behavior is similar to basic, except:
+ *
+ * It can be triggered by the system. If it's system triggered we only show it until the user makes
+ * one decision. We don't make the user show it twice.
+ *
+ * It can't be explicitly requested from apps that don't yet target android T. If they try, we
+ * remove it entirely from the request, do not return a result, and take no action on it.
+ *
+ */
+object NotificationGrantBehavior : GrantBehavior() {
+    override fun getPrompt(
+        group: LightAppPermGroup,
+        requestedPerms: Set<String>,
+        isSystemTriggeredPrompt: Boolean
+    ): Prompt {
+        val isPreT = group.packageInfo.targetSdkVersion < TIRAMISU
+        if (!isSystemTriggeredPrompt && isPreT) {
+            // Apps targeting below T cannot manually request Notifications
+            return Prompt.NO_UI_FILTER_THIS_GROUP
+        } else if (isPreT && group.isUserSet) {
+            // If the user has seen the system-triggered prompt once, don't show it again
+            return Prompt.NO_UI_FILTER_THIS_GROUP
+        }
+        return BasicGrantBehavior.getPrompt(group, requestedPerms, isSystemTriggeredPrompt)
+    }
+
+    override fun getDenyButton(
+        group: LightAppPermGroup,
+        requestedPerms: Set<String>,
+        prompt: Prompt
+    ): DenyButton {
+        return BasicGrantBehavior.getDenyButton(group, requestedPerms, prompt)
+    }
+}
---
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/model/grantPermissions/StorageGrantBehavior.kt
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.permission.ui.model.grantPermissions
+
+import android.Manifest.permission.ACCESS_MEDIA_LOCATION
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+import android.Manifest.permission_group.READ_MEDIA_AURAL
+import android.Manifest.permission_group.READ_MEDIA_VISUAL
+import android.Manifest.permission_group.STORAGE
+import android.os.Build
+import com.android.modules.utils.build.SdkLevel
+import com.android.permissioncontroller.permission.model.livedatatypes.LightAppPermGroup
+import com.android.permissioncontroller.permission.ui.model.DenyButton
+import com.android.permissioncontroller.permission.ui.model.Prompt
+import com.android.permissioncontroller.permission.utils.KotlinUtils.isPhotoPickerPromptEnabled
+
+/**
+ * Storage split from one group (STORAGE) into two (READ_MEDIA_VISUAL and READ_MEDIA_AURAL) in T.
+ * There are special dialogs to deal with a pre-T app requesting STORAGE on a post-T device. In U,
+ * the READ_MEDIA_VISUAL group was augmented with the option to select photos, which led to several
+ * new dialogs to handle that.
+ */
+object StorageGrantBehavior : GrantBehavior() {
+    override fun getPrompt(
+        group: LightAppPermGroup,
+        requestedPerms: Set<String>,
+        isSystemTriggeredPrompt: Boolean
+    ): Prompt {
+        val appSupportsSplitStoragePermissions = appSupportsSplitStoragePermissions(group)
+        if (!SdkLevel.isAtLeastT()) {
+            return Prompt.BASIC
+        } else if (appSupportsSplitStoragePermissions && group.permGroupName == STORAGE) {
+            return Prompt.NO_UI_REJECT_THIS_GROUP
+        }
+
+        if (appSupportsSplitStoragePermissions && (group.permGroupName == READ_MEDIA_AURAL ||
+            !SdkLevel.isAtLeastU())) {
+            return Prompt.BASIC
+        }
+
+        if (!appSupportsSplitStoragePermissions) {
+            if (group.packageInfo.targetSdkVersion < Build.VERSION_CODES.Q) {
+                return Prompt.STORAGE_SUPERGROUP_PRE_Q
+            } else {
+                return Prompt.STORAGE_SUPERGROUP_Q_TO_S
+            }
+        }
+
+        // Else, app supports the new photo picker dialog
+        if (requestedPerms.all { it in getPartialGrantPermissions(group) }) {
+            // Do not allow apps to request only READ_MEDIA_VISUAL_USER_SELECTED
+            return Prompt.NO_UI_REJECT_THIS_GROUP
+        }
+
+        val userSelectedPerm = group.permissions[READ_MEDIA_VISUAL_USER_SELECTED]
+        if (userSelectedPerm?.isUserFixed == true && userSelectedPerm.isGrantedIncludingAppOp) {
+            return Prompt.NO_UI_PHOTO_PICKER_REDIRECT
+        }
+
+        if (userSelectedPerm?.isGrantedIncludingAppOp == true) {
+            return Prompt.SELECT_MORE_PHOTOS
+        } else {
+            return Prompt.SELECT_PHOTOS
+        }
+    }
+
+    override fun getDenyButton(
+        group: LightAppPermGroup,
+        requestedPerms: Set<String>,
+        prompt: Prompt
+    ): DenyButton {
+        if (prompt == Prompt.SELECT_MORE_PHOTOS) {
+            return DenyButton.DONT_SELECT_MORE
+        }
+        return BasicGrantBehavior.getDenyButton(group, requestedPerms, prompt)
+    }
+
+    override fun isGroupFullyGranted(
+        group: LightAppPermGroup,
+        requestedPerms: Set<String>
+    ): Boolean {
+        if (!shouldShowPhotoPickerPrompt(group)) {
+            return super.isGroupFullyGranted(group, requestedPerms)
+        }
+
+        return group.permissions.values.any {
+            it.name !in getPartialGrantPermissions(group) && it.isGrantedIncludingAppOp
+        }
+    }
+
+    override fun isPermissionFixed(group: LightAppPermGroup, perm: String): Boolean {
+        val userSelectedPerm = group.permissions[READ_MEDIA_VISUAL_USER_SELECTED]
+        if (userSelectedPerm != null && userSelectedPerm.isGrantedIncludingAppOp &&
+            userSelectedPerm.isUserFixed) {
+            // If the user selected permission is fixed and granted, we immediately show the
+            // photo picker, rather than filtering
+            return false
+        }
+        return super.isPermissionFixed(group, perm)
+    }
+
+    private fun appSupportsSplitStoragePermissions(group: LightAppPermGroup) =
+        SdkLevel.isAtLeastT() && group.packageInfo.targetSdkVersion >= Build.VERSION_CODES.TIRAMISU
+
+    private fun shouldShowPhotoPickerPrompt(group: LightAppPermGroup) =
+        isPhotoPickerPromptEnabled() && group.permGroupName == READ_MEDIA_VISUAL &&
+            appSupportsSplitStoragePermissions(group)
+
+    private fun appSupportsPhotoPicker(group: LightAppPermGroup) =
+        shouldShowPhotoPickerPrompt(group) &&
+            group.permissions[READ_MEDIA_VISUAL_USER_SELECTED]?.isImplicit == false
+
+    private fun getPartialGrantPermissions(group: LightAppPermGroup): Set<String> {
+        return if (appSupportsPhotoPicker(group)) {
+            setOf(READ_MEDIA_VISUAL_USER_SELECTED, ACCESS_MEDIA_LOCATION)
+        } else {
+            setOf(READ_MEDIA_VISUAL_USER_SELECTED)
+        }
+    }
+}
