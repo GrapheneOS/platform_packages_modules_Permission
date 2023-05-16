c84de6a86f7584d99fe0863f5582f912eef0377e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 18:59:19 +0300

    Contact Scopes configuration UI

--- PermissionController/src/com/android/permissioncontroller/permission/ui/handheld/ExtraPermissionLink.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/handheld/ExtraPermissionLink.kt
@@ -3,6 +3,7 @@ package com.android.permissioncontroller.permission.ui.handheld
 import android.content.Context
 import android.content.pm.GosPackageState
 import android.widget.Button
+import com.android.permissioncontroller.cscopes.ContactScopesLinks
 import com.android.permissioncontroller.permission.ui.GrantPermissionsActivity
 import com.android.permissioncontroller.sscopes.StorageScopesLinks
 
@@ -22,6 +23,7 @@ abstract class ExtraPermissionLink {
 
 private val allExtraPermissionLinks = arrayOf(
         StorageScopesLinks,
+        ContactScopesLinks,
 )
 
 fun getExtraPermissionLink(ctx: Context, packageName: String, groupName: String): ExtraPermissionLink? {
