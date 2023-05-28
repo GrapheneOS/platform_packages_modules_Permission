4be48bf1b8a0168cc89f4163d38e797098da9cb8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-28 20:48:40 +0300

    Contact Scopes configuration UI

---
+++ PermissionController/src/com/android/permissioncontroller/cscopes/ContactScopesActivity.kt
@@ -0,0 +1,8 @@
+package com.android.permissioncontroller.cscopes
+
+import com.android.permissioncontroller.R
+import com.android.permissioncontroller.ext.PackageExtraConfigActivity
+
+class ContactScopesActivity : PackageExtraConfigActivity() {
+    override fun getNavGraphStart() = R.id.contact_scopes
+}
--- PermissionController/src/com/android/permissioncontroller/permission/ui/GrantPermissionsActivity.java
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/GrantPermissionsActivity.java
@@ -75,6 +75,7 @@ import androidx.core.util.Preconditions;
 import com.android.modules.utils.build.SdkLevel;
 import com.android.permissioncontroller.DeviceUtils;
 import com.android.permissioncontroller.R;
+import com.android.permissioncontroller.cscopes.ContactScopesUtils;
 import com.android.permissioncontroller.permission.model.livedatatypes.LightPermGroupInfo;
 import com.android.permissioncontroller.permission.ui.auto.GrantPermissionsAutoViewHandler;
 import com.android.permissioncontroller.permission.ui.handheld.ExtraPermissionLinkKt;
@@ -754,12 +755,21 @@ public class GrantPermissionsActivity extends SettingsActivity
 
 
     public static final int REQ_CODE_SETUP_STORAGE_SCOPES = 100;
+    public static final int REQ_CODE_SETUP_CONTACT_SCOPES = 101;
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQ_CODE_SETUP_STORAGE_SCOPES) {
             if (StorageScopesUtils.storageScopesEnabled(mTargetPackage)) {
+                setResultAndFinish();
+            }
+            return;
+        }
+
+        if (requestCode == REQ_CODE_SETUP_CONTACT_SCOPES) {
+            if (ContactScopesUtils.isContactScopesEnabled(mTargetPackage)) {
                 setResultAndFinish();
             }
             return;
