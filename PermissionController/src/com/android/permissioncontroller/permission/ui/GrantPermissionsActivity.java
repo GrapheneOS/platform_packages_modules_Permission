fc7bea618407c3bdebbcb0845e3034e71999bcd4
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-28 20:48:40 +0300

    Contact Scopes configuration UI

--- PermissionController/src/com/android/permissioncontroller/permission/ui/GrantPermissionsActivity.java
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/GrantPermissionsActivity.java
@@ -85,6 +85,7 @@ import androidx.core.util.Preconditions;
 import com.android.modules.utils.build.SdkLevel;
 import com.android.permissioncontroller.DeviceUtils;
 import com.android.permissioncontroller.R;
+import com.android.permissioncontroller.cscopes.ContactScopesUtils;
 import com.android.permissioncontroller.ecm.EnhancedConfirmationStatsLogUtils;
 import com.android.permissioncontroller.permission.model.livedatatypes.LightPermGroupInfo;
 import com.android.permissioncontroller.permission.ui.auto.GrantPermissionsAutoViewHandler;
@@ -979,6 +980,7 @@ public class GrantPermissionsActivity extends SettingsActivity
 
 
     public static final int REQ_CODE_SETUP_STORAGE_SCOPES = 100;
+    public static final int REQ_CODE_SETUP_CONTACT_SCOPES = 101;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -992,6 +994,13 @@ public class GrantPermissionsActivity extends SettingsActivity
 
         if (requestCode == REQ_CODE_SETUP_STORAGE_SCOPES) {
             if (StorageScopesUtils.isStorageScopesEnabled(this, mTargetPackage)) {
+                setResultAndFinish();
+            }
+            return;
+        }
+
+        if (requestCode == REQ_CODE_SETUP_CONTACT_SCOPES) {
+            if (ContactScopesUtils.isContactScopesEnabled(mTargetPackage, getUser())) {
                 setResultAndFinish();
             }
             return;
