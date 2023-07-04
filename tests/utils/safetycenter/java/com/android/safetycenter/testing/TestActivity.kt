fb01140a104810ded505c5cb95ad7632635637e8
Simon Wingrove <simonjw@google.com>
2023-07-04 13:08:43 +0100

    Add activity-alias to TEST_ACTIVITY of higher prio
    This hopefully allows our test to resolve only activities in the current
    test, and not resolve to intent filters from accidentally left behind
    other test APKs that might be around.  We need to keep the intents as
    implicit as this is what we are testing.
    
    Test: atest SafetyCenterActivityTest
    Bug: 287065919
    Relnote: N/A
    Change-Id: Ife3209ab4618568d959bd0a2d6f6d0935e3bd2b5

--- tests/utils/safetycenter/java/com/android/safetycenter/testing/TestActivity.kt
+++ tests/utils/safetycenter/java/com/android/safetycenter/testing/TestActivity.kt
@@ -16,9 +16,15 @@
 package com.android.safetycenter.testing
 
 import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+import android.content.pm.PackageManager.DONT_KILL_APP
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.test.core.app.ApplicationProvider
 
 /** An activity used in tests to assert the redirects. */
 class TestActivity : Activity() {
@@ -31,5 +37,33 @@ class TestActivity : Activity() {
 
         val exitButton: View? = findViewById(R.id.button)
         exitButton?.setOnClickListener { finish() }
+    }
+
+    companion object {
+
+        /**
+         * Enable a higher-priority alias of TestActivity.
+         *
+         * <p>We have seen flakes where implicit intents for TEST_ACTIVITY fail owing to multiple
+         * receivers, perhaps due to an older CTS APK hanging around. This component should be
+         * turned on (and off in tidyup) in tests in the hope of only resolving to the actively
+         * running test in these cases.
+         */
+        fun enableHighPriorityAlias() {
+            setAliasEnabledState(COMPONENT_ENABLED_STATE_ENABLED)
+        }
+        /** @see [enableHighPriorityAlias] */
+        fun disableHighPriorityAlias() {
+            setAliasEnabledState(COMPONENT_ENABLED_STATE_DISABLED)
+        }
+        private fun setAliasEnabledState(state: Int) {
+            val name =
+                ComponentName(getApplicationContext(), TestActivity::class.java.name + "Priority")
+            getApplicationContext()
+                .packageManager
+                .setComponentEnabledSetting(name, state, DONT_KILL_APP)
+        }
+
+        private fun getApplicationContext(): Context = ApplicationProvider.getApplicationContext()
     }
 }
