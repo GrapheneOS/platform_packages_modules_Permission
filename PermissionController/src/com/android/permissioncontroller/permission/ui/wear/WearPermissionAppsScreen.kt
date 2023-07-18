c5336ce31953995764ae29b2f8020ccbf4a8b27a
YoungJoon Yang <youngjoonyang@google.com>
2023-07-18 20:21:49 +0900

    Implement WearPermissionAppsFragment
    After: https://screenshot.googleplex.com/9gyyir8n4HMTZFh.png
    
    Bug: 290012979
    Bug: 291683361
    Test: Settings -> Location -> App location permissions
    Change-Id: I54b257b6e2178b72f8dc40c96ce66cc95c60b7ef

---
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/wear/WearPermissionAppsScreen.kt
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.permission.ui.wear
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.android.permissioncontroller.R
+import com.android.permissioncontroller.permission.ui.Category
+import com.android.permissioncontroller.permission.ui.wear.elements.Chip
+import com.android.permissioncontroller.permission.ui.wear.elements.ListHeader
+import com.android.permissioncontroller.permission.ui.wear.elements.ScrollableScreen
+
+/**
+ * Compose the screen associated to a [WearPermissionAppsFragment].
+ */
+@Composable
+fun WearPermissionAppsScreen(
+    helper: WearPermissionsAppHelper
+) {
+    val categorizedApps = helper.categorizedAppsLiveData().observeAsState(emptyMap())
+    val hasSystemApps = helper.hasSystemAppsLiveData().observeAsState(false)
+    val showSystem = helper.shouldShowSystemLiveData().observeAsState(false)
+    var isLoading by remember { mutableStateOf(true) }
+
+    val title = helper.getTitle()
+    val subTitle = helper.getSubTitle()
+    val showAlways = helper.showAlways()
+    val chipsByCategory = helper.getChipsByCategory(categorizedApps.value)
+
+    WearPermissionAppsContent(
+        chipsByCategory,
+        showSystem.value,
+        hasSystemApps.value,
+        title,
+        subTitle,
+        showAlways,
+        isLoading,
+        helper.onShowSystemClick
+    )
+
+    if (isLoading && categorizedApps.value.isNotEmpty()) {
+        isLoading = false
+    }
+    helper.setCreationLogged(true)
+}
+
+@Composable
+internal fun WearPermissionAppsContent(
+    chipsByCategory: Map<String, List<ChipInfo>>,
+    showSystem: Boolean,
+    hasSystemApps: Boolean,
+    title: String,
+    subtitle: String,
+    showAlways: Boolean,
+    isLoading: Boolean,
+    onShowSystemClick: (showSystem: Boolean) -> Unit
+) {
+    ScrollableScreen (
+        title = title,
+        subtitle = subtitle,
+        isLoading = isLoading
+    ) {
+        for (category in categoryOrder) {
+            val chips = chipsByCategory[category]
+            if (chips.isNullOrEmpty()) {
+                continue
+            }
+            item {
+                ListHeader(getCategoryString(category, showAlways))
+            }
+            chips.forEach {
+                item {
+                    Chip(
+                        label = it.title,
+                        icon = it.icon,
+                        onClick = {
+                            it.onClick()
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+
+        if (hasSystemApps) {
+            item {
+                Chip(
+                    label = if (showSystem) {
+                        stringResource(R.string.menu_hide_system)
+                    } else {
+                        stringResource(R.string.menu_show_system)
+                    },
+                    onClick = {
+                        onShowSystemClick(!showSystem)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
+
+internal fun getCategoryString(category: String, showAlways: Boolean) =
+    when (category) {
+        "allowed_storage_full" -> R.string.allowed_storage_full
+        "allowed_storage_scoped" -> R.string.allowed_storage_scoped
+        Category.ALLOWED.categoryName ->
+            if (showAlways) {
+                R.string.allowed_always_header
+            } else {
+                R.string.allowed_header
+            }
+        Category.ALLOWED_FOREGROUND.categoryName -> R.string.allowed_foreground_header
+        Category.ASK.categoryName -> R.string.ask_header
+        Category.DENIED.categoryName -> R.string.denied_header
+        else -> throw IllegalArgumentException("Wrong category: $category")
+    }
+
+internal val categoryOrder = listOf(
+    "allowed_storage_full",
+    "allowed_storage_scoped",
+    Category.ALLOWED.categoryName,
+    Category.ALLOWED_FOREGROUND.categoryName,
+    Category.ASK.categoryName,
+    Category.DENIED.categoryName
+)
---
+++ PermissionController/src/com/android/permissioncontroller/permission/ui/wear/elements/Chip.kt
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.permissioncontroller.permission.ui.wear.elements
+
+import android.graphics.drawable.Drawable
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipColors
+import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+
+/**
+ * This component is an alternative to [Chip], providing the following:
+ * - a convenient way of providing a label and a secondary label;
+ * - a convenient way of providing an icon, and choosing their size based on the
+ * sizes recommended by the Wear guidelines;
+ */
+@Composable
+public fun Chip(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    secondaryLabel: String? = null,
+    icon: Any? = null,
+    iconContentDescription: String? = null,
+    largeIcon: Boolean = false,
+    textColor: Color = MaterialTheme.colors.onSurface,
+    iconColor: Color = Color.Unspecified,
+    colors: ChipColors = ChipDefaults.secondaryChipColors(),
+    enabled: Boolean = true
+) {
+    val iconParam: (@Composable BoxScope.() -> Unit)? =
+        icon?.let {
+            {
+                val iconSize = if (largeIcon) {
+                    ChipDefaults.LargeIconSize
+                } else {
+                    ChipDefaults.IconSize
+                }
+
+                Row {
+                    val iconModifier = Modifier
+                        .size(iconSize)
+                        .clip(CircleShape)
+                    when (icon) {
+                        is ImageVector ->
+                            Icon(
+                                imageVector = icon,
+                                tint = iconColor,
+                                contentDescription = iconContentDescription,
+                                modifier = iconModifier
+                            )
+
+                        is Int ->
+                            Icon(
+                                painter = painterResource(id = icon),
+                                tint = iconColor,
+                                contentDescription = iconContentDescription,
+                                modifier = iconModifier
+                            )
+                        is Drawable ->
+                            Icon(
+                                painter = rememberDrawablePainter(icon),
+                                tint = iconColor,
+                                contentDescription = iconContentDescription,
+                                modifier = iconModifier
+                            )
+                        else -> {
+                        }
+                    }
+                }
+            }
+        }
+
+    Chip(
+        label = label,
+        onClick = onClick,
+        modifier = modifier,
+        secondaryLabel = secondaryLabel,
+        icon = iconParam,
+        largeIcon = largeIcon,
+        textColor = textColor,
+        colors = colors,
+        enabled = enabled
+    )
+}
+
+/**
+ * This component is an alternative to [Chip], providing the following:
+ * - a convenient way of providing a label and a secondary label;
+ * - a convenient way of providing an icon, and choosing their size based on the
+ * sizes recommended by the Wear guidelines;
+ */
+@Composable
+public fun Chip(
+    @StringRes labelId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    @StringRes secondaryLabel: Int? = null,
+    icon: Any? = null,
+    largeIcon: Boolean = false,
+    textColor: Color = MaterialTheme.colors.onSurface,
+    iconColor: Color = Color.Unspecified,
+    colors: ChipColors = ChipDefaults.secondaryChipColors(),
+    enabled: Boolean = true
+) {
+    Chip(
+        label = stringResource(id = labelId),
+        onClick = onClick,
+        modifier = modifier,
+        secondaryLabel = secondaryLabel?.let { stringResource(id = it) },
+        icon = icon,
+        largeIcon = largeIcon,
+        textColor = textColor,
+        iconColor = iconColor,
+        colors = colors,
+        enabled = enabled
+    )
+}
+
+/**
+ * This component is an alternative to [Chip], providing the following:
+ * - a convenient way of providing a label and a secondary label;
+ */
+@Composable
+public fun Chip(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    secondaryLabel: String? = null,
+    icon: (@Composable BoxScope.() -> Unit)? = null,
+    largeIcon: Boolean = false,
+    textColor: Color = MaterialTheme.colors.onSurface,
+    colors: ChipColors = ChipDefaults.secondaryChipColors(),
+    enabled: Boolean = true
+) {
+    val hasSecondaryLabel = secondaryLabel != null
+    val hasIcon = icon != null
+
+    val labelParam: (@Composable RowScope.() -> Unit) =
+        {
+            Text(
+                text = label,
+                color = textColor,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = if (hasSecondaryLabel || hasIcon) TextAlign.Start else TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = if (hasSecondaryLabel) 1 else 2,
+                style = MaterialTheme.typography.button
+            )
+        }
+
+    val secondaryLabelParam: (@Composable RowScope.() -> Unit)? =
+        secondaryLabel?.let {
+            {
+                Text(
+                    text = secondaryLabel,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.caption2
+                )
+            }
+        }
+
+    val contentPadding = if (largeIcon) {
+        val verticalPadding = ChipDefaults.ChipVerticalPadding
+        PaddingValues(
+            start = 10.dp,
+            top = verticalPadding,
+            end = ChipDefaults.ChipHorizontalPadding,
+            bottom = verticalPadding
+        )
+    } else {
+        ChipDefaults.ContentPadding
+    }
+
+    Chip(
+        label = labelParam,
+        onClick = onClick,
+        modifier = modifier
+            .adjustChipHeightToFontScale(LocalConfiguration.current.fontScale)
+            .fillMaxWidth(),
+        secondaryLabel = secondaryLabelParam,
+        icon = icon,
+        colors = colors,
+        enabled = enabled,
+        contentPadding = contentPadding
+    )
+}
