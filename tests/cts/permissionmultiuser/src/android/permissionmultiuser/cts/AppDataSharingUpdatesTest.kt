fbd4426c0a449ce99fd2fe77767963cb1191f9fc
Karishma Vakil <kvakil@google.com>
2023-09-11 18:18:08 +0000

    [Safety Labels] Make multiuser tests more reliable
    We are noticing test failures on slower devices. To alleviate that:
    * increase waiting time as slow devices don't open the activity in time
      for the assertions
    * remove some unnecessary assertions which are covered in single user
      variant
    * add exclude annotations for tests so that tests which will have
      assumptions failed don't run at all
    * include selector information in assertion message
    
    Bug: 281550547
    Test: atest CtsPermissionMultiUserTestCases
    Change-Id: Ie77d8c8782fde02226c5f9ff07c9f4ca0551c68e

--- tests/cts/permissionmultiuser/src/android/permissionmultiuser/cts/AppDataSharingUpdatesTest.kt
+++ tests/cts/permissionmultiuser/src/android/permissionmultiuser/cts/AppDataSharingUpdatesTest.kt
@@ -412,11 +412,8 @@ class AppDataSharingUpdatesTest {
 
         private fun assertNoUpdatesPresent() {
             findView(By.descContains(DATA_SHARING_UPDATES), true)
-            findView(By.textContains(DATA_SHARING_UPDATES_SUBTITLE), true)
             findView(By.textContains(DATA_SHARING_NO_UPDATES_MESSAGE), true)
             findView(By.textContains(LOCATION_PACKAGE_NAME_SUBSTRING), false)
-            findView(By.textContains(UPDATES_IN_LAST_30_DAYS), false)
-            findView(By.textContains(DATA_SHARING_UPDATES_FOOTER_MESSAGE), true)
         }
 
         private fun grantLocationPermission(packageName: String) {
@@ -444,7 +441,7 @@ class AppDataSharingUpdatesTest {
         private fun findView(selector: BySelector, expected: Boolean) {
             val timeoutMillis =
                 if (expected) {
-                    10000L
+                    20000L
                 } else {
                     1000L
                 }
@@ -467,7 +464,13 @@ class AppDataSharingUpdatesTest {
                 } catch (e: Exception) {
                     e
                 }
-            Assert.assertTrue("Expected to find view: $expected", (exception == null) == expected)
+            val actual = exception == null
+            val message = if (expected) {
+                "Expected view $selector not found"
+            } else {
+                "Unexpected view found: $selector"
+            }
+            Assert.assertTrue(message, actual == expected)
         }
     }
 }
