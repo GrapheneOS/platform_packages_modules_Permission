c4c2b0e7896ee2a0bed2a53e8ab68c5394b78eaa
Kevin Han <kevhan@google.com>
2023-09-20 19:47:58 +0000

    Fix Transformations.map not being on main
    Fix IllegalStateException from new AndroidX SDK drop
    
    Bug: 301546417
    Fix: 301546417
    Test: atest AppHibernationIntegrationTest
    Merged-In: Ie64330e5bbf7fa582bc23622dbe026721bb9be8c
    Change-Id: I41fe35cd61ceda3a25d7f9057a5aaf3f8a2e54fa

--- PermissionController/src/com/android/permissioncontroller/permission/service/PermissionControllerServiceModel.kt
+++ PermissionController/src/com/android/permissioncontroller/permission/service/PermissionControllerServiceModel.kt
@@ -287,10 +287,14 @@ class PermissionControllerServiceModel(private val service: PermissionController
     fun onCountUnusedApps(
         callback: IntConsumer
     ) {
-        val unusedAppsCount = Transformations.map(getUnusedPackages()) {
-            it?.size ?: 0
+        GlobalScope.launch(Main.immediate) {
+            val unusedAppsCount = Transformations.map(getUnusedPackages()) {
+                it?.size ?: 0
+            }
+            observeAndCheckForLifecycleState(unusedAppsCount) { count ->
+                callback.accept(count ?: 0)
+            }
         }
-        observeAndCheckForLifecycleState(unusedAppsCount) { count -> callback.accept(count ?: 0) }
     }
 
     /**
