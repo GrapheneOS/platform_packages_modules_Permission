b7d9f4fff003f75841e38747f1717fddb8757c86
Tyler Dewey <deweytyl@google.com>
2023-10-24 20:10:24 +0000

    Add some debug logging for SC text fade animations.
    Bug: 283953501
    Test: treehugger
    Relnote: N/a
    Change-Id: Ic88c0d3be54861002e7ca381fbdf9fa72d95ff08

--- PermissionController/src/com/android/permissioncontroller/safetycenter/ui/SafetyStatusPreference.java
+++ PermissionController/src/com/android/permissioncontroller/safetycenter/ui/SafetyStatusPreference.java
@@ -75,7 +75,7 @@ public class SafetyStatusPreference extends Preference implements ComparablePref
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
-        Log.d(TAG, String.format("onBindViewHolder called for status %s", mStatus));
+        Log.v(TAG, String.format("onBindViewHolder called for status %s", mStatus));
 
         if (mStatus == null) {
             return;
@@ -144,12 +144,14 @@ public class SafetyStatusPreference extends Preference implements ComparablePref
         if (mIsTextChangeAnimationRunning) {
             return;
         }
+        Log.v(TAG, "Starting status text animation");
         String titleText = mStatus.getTitle().toString();
         String summaryText = mStatus.getSummary(getContext()).toString();
         boolean titleEquals = titleView.getText().toString().equals(titleText);
         boolean summaryEquals = summaryView.getText().toString().equals(summaryText);
         Runnable onFinish =
                 () -> {
+                    Log.v(TAG, "Finishing status text animation");
                     mIsTextChangeAnimationRunning = false;
                     runTextAnimationIfNeeded(titleView, summaryView);
                 };
@@ -339,7 +341,7 @@ public class SafetyStatusPreference extends Preference implements ComparablePref
 
     void setData(StatusUiData statusUiData) {
         mStatus = statusUiData;
-        Log.d(TAG, String.format("setData called for status %s", mStatus));
+        Log.v(TAG, String.format("setData called for status %s", mStatus));
         safeNotifyChanged();
     }
 
@@ -357,7 +359,7 @@ public class SafetyStatusPreference extends Preference implements ComparablePref
         new Handler(Looper.getMainLooper())
                 .post(
                         () -> {
-                            Log.d(
+                            Log.v(
                                     TAG,
                                     String.format("Calling notifyChanged for status %s", mStatus));
                             notifyChanged();
