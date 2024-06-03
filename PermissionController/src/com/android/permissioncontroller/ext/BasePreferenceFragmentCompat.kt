0a6ec4a52b7538cae7cd7edffcc101a19303d43e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-03 13:13:04 +0300

    temp: suppress deprecation errors caused by android.R.id.home option item

--- PermissionController/src/com/android/permissioncontroller/ext/BasePreferenceFragmentCompat.kt
+++ PermissionController/src/com/android/permissioncontroller/ext/BasePreferenceFragmentCompat.kt
@@ -13,6 +13,7 @@ abstract class BasePreferenceFragmentCompat : PreferenceFragmentCompat() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        @Suppress("DEPRECATION") // see onOptionsItemSelected
         setHasOptionsMenu(true) // needed for the "back arrow" button even when there's no menu
     }
 
@@ -38,6 +39,10 @@ abstract class BasePreferenceFragmentCompat : PreferenceFragmentCompat() {
         requireActivity().title = getTitle()
     }
 
+    // it's not clear how to resolve deprecation warnings for setHasOptionsMenu and onOptionsItemSelected,
+    // they are suppressed in upstream fragments that use android.R.id.home too
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
             this.pressBack()
